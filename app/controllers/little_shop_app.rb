3bcad57c44c170be79b25f9980d185103b3aae12
Mike McKee <mikecm@gmail.com>
2018-08-15 18:51:34 -0600

    ADDS edit functionality for merchants

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -1,4 +1,6 @@
 class LittleShopApp < Sinatra::Base
+  set :method_override, true
+
   get '/merchants' do
     @merchants = Merchant.all
     erb :'merchants/index'
@@ -7,5 +9,15 @@ class LittleShopApp < Sinatra::Base
   get '/merchants/:id' do
     @merchant = Merchant.find(params[:id])
     erb :'merchants/show'
+  end
+
+  get '/merchants/:id/edit' do
+    @merchant = Merchant.find(params[:id])
+    erb :'merchants/edit'
+  end
+
+  put '/merchants/:id' do |id|
+    Merchant.update(id.to_i, params[:merchant])
+    redirect '/merchants'
   end
 end
