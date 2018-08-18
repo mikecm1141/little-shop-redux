3c592b4e783800a9433e097e82c366cc9706ee95
Nick Lindeberg <lindebergnick@gmail.com>
2018-08-18 12:21:09 -0600

    TEST and IMPLEMENTS validations for item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,6 +2,10 @@ RSpec.describe Item,type: :model do
   describe 'Validations' do
 
     it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:description)}
+    it {should validate_presence_of(:unit_price)}
+    it {should validate_presence_of(:merchant_id)}
+    it {should validate_presence_of(:image)}
   
   end
 end
