fa0ae5a6af8d4019c89f1e2bb6182b2a7422d75b
Nick Lindeberg <lindebergnick@gmail.com>
2018-08-19 14:20:38 -0600

    ADDS seed for items table

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,16 @@ CSV.foreach('./data/merchants.csv', headers: true, header_converters: :symbol) d
     updated_at: row[:updated_at]
   )
 end
+
+CSV.foreach('./data/items.csv'), headers: true, header_converters: :symbol) do |row|
+  Item.create(
+    id:          row[:id],
+    name:        row[:name],
+    description: row[:description],
+    unit_price:  row[:unit_price],
+    merchant_id: row[:merchant_id],
+    image:       "https://i.redd.it/51dcpxhdwcox.jpg",
+    created_at:  row[:created_at],
+    updated_at:  row[:updated_at]
+  )
+end
