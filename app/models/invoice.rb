4085c83935a16b287fdee933f21e89896fefe5ce
Mike McKee <mikecm@gmail.com>
2018-08-21 10:17:12 -0600

    TESTS and IMPLEMENTS welcome page

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,8 @@
 class Invoice < ActiveRecord::Base
   belongs_to :merchant
   validates_presence_of :customer_id, :merchant_id, :status
+
+  def self.last_updated
+    maximum(:updated_at)
+  end
 end
